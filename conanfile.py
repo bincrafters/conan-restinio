941faf43a455aedde109c27a664ff307acc06efb
Kyle Macfarlan <kyle.macfarlan@virtualmaterials.com>
2018-10-07 21:40:43 -0500

    initial commit

---
+++ conanfile.py
@@ -0,0 +1,28 @@
+from conans import ConanFile, CMake, tools
+
+
+class RestinioConan(ConanFile):
+    name = "restinio"
+    version = "0.4.8"
+    license = "BSD-3"
+    url = "https://bitbucket.org/sobjectizerteam/restinio-0.4"
+    description = "RESTinio is a header-only C++14 library that gives you an embedded HTTP/Websocket server."
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    generators = "cmake"
+    requires = "asio/1.12.0@bincrafters/stable", \
+                "fmt/5.2.0@bincrafters/stable", \
+                "http-parser/2.8.1@bincrafters/stable"
+
+    def source(self):
+        self.run("git clone https://github.com/Stiffstream/restinio.git")
+        self.run("cd restinio && git checkout v.0.4.8")
+
+    def package(self):
+        self.copy("*.hpp", dst="include/restinio", src="restinio/dev/restinio")
+        self.copy("*.inl", dst="include/restinio", src="restinio/dev/restinio")
+
+    def package_id(self):
+        self.info.header_only()
+

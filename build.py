b013c36665eb759ea24f1a3f3940c3b07eec25d8
Kyle Macfarlan <kyle.macfarlan@virtualmaterials.com>
2018-10-07 22:43:57 -0500

    set pure_c flag to false so we build with libstdc++11

--- build.py
+++ build.py
@@ -3,5 +3,5 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(build_policy = "missing")
-    builder.add_common_builds()
+    builder.add_common_builds(pure_c=False)
     builder.run()

46f73f9f4b8c00ea8501b534eea5b3d1412e4852
Uilian Ries <uilianries@gmail.com>
2019-03-22 11:36:57 -0300

    Bump version to 0.4.8.6

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conans import ConanFile, tools
 
 class RestinioConan(ConanFile):
     name = "restinio"
-    version = "0.4.8"
+    version = "0.4.8.6"
     license = "BSD-3-Clause"
     homepage = "https://github.com/Stiffstream/restinio"
     url = "https://github.com/bincrafters/conan-restinio"

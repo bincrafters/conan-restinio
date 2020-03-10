e972cb97a3ceea92705144e8eb9d146b83d6d433
Croydon <git@cr0ydon.com>
2020-03-10 16:19:38 +0100

    Update to 0.6.5

--- conanfile.py
+++ conanfile.py
@@ -4,22 +4,21 @@ from conans import ConanFile, tools
 
 class RestinioConan(ConanFile):
     name = "restinio"
-    version = "0.4.8.6"
     license = "BSD-3-Clause"
     homepage = "https://github.com/Stiffstream/restinio"
     url = "https://github.com/bincrafters/conan-restinio"
     topics = ("conan", "restinio", "http-server", "websocket", "http")
     description = "RESTinio is a header-only C++14 library that gives you an embedded HTTP/Websocket server."
-    exports = ["LICENSE.md"]
     no_copy_source = True
     requires = ("asio/1.13.0",
-                "fmt/5.2.0@bincrafters/stable",
+                "fmt/5.3.0",
                 "http_parser/2.9.2")
     _source_subfolder = "source_subfolder"
 
     def source(self):
-        tools.get("{}/archive/v.{}.tar.gz".format(self.homepage, self.version))
-        os.rename("{}-v.{}".format(self.name, self.version), self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
 
     def package(self):
         self.copy(pattern="LICENSE*", dst="licenses", src=self._source_subfolder)
